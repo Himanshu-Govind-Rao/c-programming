96129932957205a7b24a2ea3f73831d9c794a541
Himanshu-Govind-Rao <himanshugovindrao13@gmail.com>
2026-01-09 23:23:03 +0530

    Add Pokemon struct and initialize instances
    Define a struct for Pokemon with attributes and initialize Pikachu and Charizard.

--- structures/1Basicofstructure.c
+++ structures/1Basicofstructure.c
@@ -1,1 +1,25 @@
+#include<stdio.h>
+int main(){
+    struct pokemon{  // user defined data type
+        int hp;
+        int speed;
+        int attack;
+        char tier;
+    } pikachu , charizard ;
 
+    
+    pikachu.attack = 60;
+    pikachu.hp = 70;
+    pikachu.speed = 85;
+    pikachu.tier = 'A';
+
+    printf("%d",pikachu.attack );
+
+
+    charizard.attack = 120;
+    charizard.hp = 80;
+    charizard.speed = 90;
+    charizard.tier = 'S';
+
+    return 0;
+}  
