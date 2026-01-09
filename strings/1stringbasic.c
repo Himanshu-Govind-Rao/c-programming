735c876da8625453ccae68c7ee9ad20f0b5d64be
Himanshu-Govind-Rao <himanshugovindrao13@gmail.com>
2026-01-09 23:18:46 +0530

    Implement basic string printing in C
    Add a simple C program to print a string.

--- strings/1stringbasic.c
+++ strings/1stringbasic.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+int main(){
+    char arr[] = {'H','e','l','l','o','\0'};
+    // char ch = '\0' ;      // null character
+    // int x = 0;
+    // char a = (char)x; // a -> '\0'
+    // printf("%c",ch);
+    int i = 0;
+    while(arr[i]!='\0'){
+        printf("%c",arr[i]);
+        i++;
+    }
+    return 0;
+}
